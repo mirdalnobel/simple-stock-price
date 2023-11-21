4a8a4ff159b1b1a831ff2d7bb898153997eebe89
Mirdal Nobel Hamun <34174584+mirdalnobel@users.noreply.github.com>
2023-11-21 10:18:10 +0700

    Update simple-stock-price-app.py

--- simple-stock-price-app.py
+++ simple-stock-price-app.py
@@ -12,14 +12,17 @@ Shown are the stock **closing price** and ***volume*** of Google!
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 # Define the ticker symbol
 tickerSymbol = 'GOOGL'
-# Get data on this ticker
+
 try:
+    # Get data on this ticker
     tickerData = yf.Ticker(tickerSymbol)
+    
     # Get the historical prices for this ticker
-    tickerDf = tickerData.history(period='1d', start='2010-05-31', end='2020-05-31')
-except yf.errors.YFinanceError as yf_error:
-    st.error(f"An error occurred while fetching data from yfinance: {yf_error}")
-    st.stop()
+    try:
+        tickerDf = tickerData.history(period='1d', start='2010-05-31', end='2020-05-31')
+    except yf.errors.YFinanceError as yf_error:
+        st.error(f"An error occurred while fetching data from yfinance: {yf_error}")
+        st.stop()
 except requests.exceptions.RequestException as req_error:
     st.error(f"An error occurred in the request to yfinance: {req_error}")
     st.stop()
@@ -27,14 +30,15 @@ except Exception as e:
     st.error(f"An unexpected error occurred: {e}")
     st.stop()
 
-# Open High Low Close Volume Dividends Stock Splits
+# Check if tickerDf is defined (no error occurred)
+if 'tickerDf' in locals():
+    # Open High Low Close Volume Dividends Stock Splits
+    st.write("""
+    ## Closing Price
+    """)
+    st.line_chart(tickerDf.Close)
 
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
+    st.write("""
+    ## Volume Price
+    """)
+    st.line_chart(tickerDf.Volume)
