b4b527a5c93788d3834bdd2d91d3298478eff7e9
Mirdal Nobel Hamun <34174584+mirdalnobel@users.noreply.github.com>
2023-11-21 10:13:31 +0700

    Update simple-stock-price-app.py

--- simple-stock-price-app.py
+++ simple-stock-price-app.py
@@ -9,18 +9,24 @@ Shown are the stock **closing price** and ***volume*** of Google!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
+# Define the ticker symbol
 tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+# Get data on this ticker
+try:
+    tickerData = yf.Ticker(tickerSymbol)
+    # Get the historical prices for this ticker
+    tickerDf = tickerData.history(period='1d', start='2010-05-31', end='2020-05-31')
+except yf.errors.YFinanceError as e:
+    st.error(f"An error occurred: {e}")
+    st.stop()
+
+# Open High Low Close Volume Dividends Stock Splits
 
 st.write("""
 ## Closing Price
 """)
 st.line_chart(tickerDf.Close)
+
 st.write("""
 ## Volume Price
 """)
