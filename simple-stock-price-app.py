4d7a70a657075858403b2ff0f51cf84af7f02ca6
Mirdal Nobel Hamun <34174584+mirdalnobel@users.noreply.github.com>
2023-11-21 09:42:28 +0700

    Update simple-stock-price-app.py

--- simple-stock-price-app.py
+++ simple-stock-price-app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Shown are the stock **closing price** and ***volume*** of Google!
 
 """)
 
@@ -17,5 +17,11 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
